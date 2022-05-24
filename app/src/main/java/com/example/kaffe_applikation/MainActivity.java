791c470a1fd58243b4b9bf9bd3187df0b5109b46
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-05-24 18:30:31 +0200

    Implemented Gson with Drink class

--- app/src/main/java/com/example/kaffe_applikation/MainActivity.java
+++ app/src/main/java/com/example/kaffe_applikation/MainActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_FILE = "https://mobprog.webug.se/json-api?login=b21phiro";
+    private ArrayList<Drink> drinks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     public void onPostExecute(String json) {
         Gson gson = new Gson();
         Type type = new TypeToken<ArrayList<Drink>>(){}.getType();
-        ArrayList<Drink> drinks;
-        drinks = gson.fromJson(json, type);
+        this.drinks = gson.fromJson(json, type);
+
     }
 }
