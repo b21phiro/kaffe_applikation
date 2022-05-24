dc2798c5b5c3ceb7eb086d28ee1ae9ddfdb3e291
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-05-24 19:05:36 +0200

    RecyclerView + Adapter implementation with cards

--- app/src/main/java/com/example/kaffe_applikation/DrinkRecyclerViewAdapter.java
+++ app/src/main/java/com/example/kaffe_applikation/DrinkRecyclerViewAdapter.java
@@ -1,31 +1,54 @@
 package com.example.kaffe_applikation;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.w3c.dom.Text;
+
+import java.text.BreakIterator;
+import java.util.ArrayList;
+
 public class DrinkRecyclerViewAdapter extends RecyclerView.Adapter<DrinkRecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<Drink> drinks;
+    private LayoutInflater inflater;
+
+    public DrinkRecyclerViewAdapter(Context context, ArrayList<Drink> drinks) {
+        this.inflater = LayoutInflater.from(context);
+        this.drinks = drinks;
+    }
+
     @NonNull
     @Override
     public DrinkRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.item_row, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DrinkRecyclerViewAdapter.ViewHolder holder, int position) {
-
+        Drink drink = drinks.get(position);
+        holder.name.setText(drink.getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return drinks.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.name);
         }
     }
 }
--- app/src/main/java/com/example/kaffe_applikation/MainActivity.java
+++ app/src/main/java/com/example/kaffe_applikation/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kaffe_applikation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
+    private DrinkRecyclerViewAdapter adapter;
     private final String JSON_FILE = "https://mobprog.webug.se/json-api?login=b21phiro";
     private ArrayList<Drink> drinks;
 
@@ -31,6 +34,12 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Gson gson = new Gson();
         Type type = new TypeToken<ArrayList<Drink>>(){}.getType();
         this.drinks = gson.fromJson(json, type);
+
+        RecyclerView recyclerView = findViewById(R.id.cupHolder);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new DrinkRecyclerViewAdapter(this, this.drinks);
+        recyclerView.setAdapter(adapter);
 
     }
 }
