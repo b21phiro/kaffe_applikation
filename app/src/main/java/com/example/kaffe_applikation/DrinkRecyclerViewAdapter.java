81c5ba783ae7c1aef261bfc5848b3b213d3d5aa4
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-05-24 18:40:42 +0200

    Created RecyclerView adapter class

--- app/src/main/java/com/example/kaffe_applikation/DrinkRecyclerViewAdapter.java
+++ app/src/main/java/com/example/kaffe_applikation/DrinkRecyclerViewAdapter.java
@@ -1,4 +1,31 @@
 package com.example.kaffe_applikation;
 
-public class DrinkRecyclerViewAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DrinkRecyclerViewAdapter extends RecyclerView.Adapter<DrinkRecyclerViewAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public DrinkRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DrinkRecyclerViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
