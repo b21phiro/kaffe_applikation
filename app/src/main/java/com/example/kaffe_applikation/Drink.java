9a791164845918e61fb197d405374995a47a34db
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-05-24 18:24:28 +0200

    Created Drink class

---
+++ app/src/main/java/com/example/kaffe_applikation/Drink.java
@@ -0,0 +1,81 @@
+package com.example.kaffe_applikation;
+
+public class Drink {
+    private String id, name, type, company, location, category;
+    private Integer size, cost;
+
+    public Drink(String id, String name, String type, String company, String location, String category, Integer size, Integer cost) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.company = company;
+        this.location = location;
+        this.category = category;
+        this.size = size;
+        this.cost = cost;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}
