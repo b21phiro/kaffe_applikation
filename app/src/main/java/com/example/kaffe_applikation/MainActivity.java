593889b598e9153158f09186296171ad392d5b28
b21phiro <b21phiro@student.his.se>
2022-05-24 13:47:43 +0200

    Init

---
+++ app/src/main/java/com/example/kaffe_applikation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kaffe_applikation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
